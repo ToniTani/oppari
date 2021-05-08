3d0c22a68b6d5023171ed26c21aad22faabe8335
ToniTani <toni.tani@student.saimia.fi>
2021-05-08 03:38:18 +0300

    mainpage

---
+++ src/app/mainpage/mainpage.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { NgwWowModule } from 'ngx-wow';
+
+
+@Component({
+  selector: 'app-mainpage',
+  templateUrl: './mainpage.component.html',
+  styleUrls: ['./mainpage.component.css']
+})
+export class MainpageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
